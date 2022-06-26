024e5475fcc77ebe77eaa982b883de1627b2dd05
azadahmad1992 <azadfixahmad0709@gmail.com>
2022-06-26 18:02:11 -0400

    done change

--- src/com/upskill/java/Class_2_Loops.java
+++ src/com/upskill/java/Class_2_Loops.java
@@ -1,6 +1,11 @@
 package com.upskill.java;
 
 public class Class_2_Loops {
+	
+	
+	//test git check
+	
+	// we are hero 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
